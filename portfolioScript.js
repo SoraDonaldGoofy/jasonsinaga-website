fe67da22a9813f9d2b17a3619a68a9762ba87cba
SoraDonaldGoofy <131983849+SoraDonaldGoofy@users.noreply.github.com>
2024-01-27 20:10:53 -0600

    Update portfolioScript to change tab buttons color

--- portfolioScript.js
+++ portfolioScript.js
@@ -11,13 +11,33 @@ function openSkill(panelIndex, skill) {
 
     for (i = 0; i < tabButtons.length; i++) {
         if (tabButtons[i].style.backgroundColor!="#35435A") {
-            tabButtons[i].style.backgroundColor="#35435A";
+            tabButtons[i].style.backgroundColor="blue";
         }
     }
 
     // Change active tab button's background color
     tabButtons[panelIndex].style.backgroundColor="whitesmoke";
     tabButtons[panelIndex].style.transition="0.2s";
+
+    for (i = 0; i < tabButtons.length; i++) {
+        if (tabButtons[i].style.color!="whitesmoke") {
+            tabButtons[i].style.color="whitesmoke";
+        }
+    }
+
+    // Change active tab button's text color to black
+    tabButtons[panelIndex].style.color="black";
+
+}
+
+// Function to open about me page
+function openNav() {
+    document.getElementById("mySidenav").style.width = "750px";
+}
+  
+// Function to close about me page
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
 
 openSkill(0, "Teaching");
